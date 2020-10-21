c929c3908cd46ac7cf85bf2cd725f87471dbdd11
jean-claude07 <jeclaude78@gmail.com>
2020-10-21 11:03:44 +0300

    yarn

---
+++ front/build/precache-manifest.a43c4f5e158055656b66630be75ee4b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac628a07177021dab7471e8f4138be4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941e3b14bf1b2579fb66",
+    "url": "/static/css/main.b0b9741b.chunk.css"
+  },
+  {
+    "revision": "b09310e58c690f891fef",
+    "url": "/static/js/2.8dcbcce6.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.8dcbcce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941e3b14bf1b2579fb66",
+    "url": "/static/js/main.12891060.chunk.js"
+  },
+  {
+    "revision": "3d5ea4fe2cd9c08e86df",
+    "url": "/static/js/runtime-main.af35d6d3.js"
+  },
+  {
+    "revision": "ed6854563e886271e585cb87d4216a9d",
+    "url": "/static/media/254.ed685456.jpg"
+  }
+]);
