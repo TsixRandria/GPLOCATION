82fddcad92d8e07d8db45046bfd6a48da97b81fd
jean-claude07 <jeclaude78@gmail.com>
2020-10-23 17:18:56 +0300

    contact

--- admin/src/pages/Message/ClientMessage.js
+++ admin/src/pages/Message/ClientMessage.js
@@ -13,21 +13,25 @@ class ClientMessage extends Component {
 
     componentDidMount() {
         // Simple GET request using axios
-        axios.get(`/contacts`)
-        .then(res => {
-          const contacts = res.data;
-          this.setState({ contacts });
-          console.log(res);
-          
-        })
-
-        
+        this.getMessage();
     }
 
+    getMessage = () => {
+        axios.get(`/contacts`)
+            .then(res => {
+                const contacts = res.data;
+                this.setState({ contacts });
+                console.log(res);
+            })
+    }
     
-
-    
-
+    deleteMessage = (message) => {
+        axios.delete(`/contacts/${message.id}`).then(response => {
+            if (response.status === 204) {
+                this.getMessage();
+            }
+        })
+    }
     
     render() {
         
@@ -50,7 +54,6 @@ class ClientMessage extends Component {
                                 <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200">TELEPHONE</th>
                                 <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200" >MESSAGE</th>
                                 <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-200 rounded-tr rounded-br"></th>
-                                
                             </tr>
                             </thead>
                             <tbody>
@@ -62,7 +65,13 @@ class ClientMessage extends Component {
                                             <td className="border px-4 py-2">{ contact.email}</td>
                                             <td className="border px-4 py-2">{ contact.telephone}</td>
                                             <td className="border px-4 py-2">{ contact.message}</td> 
-                                            <td className="border px-4 py-2"><button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded" >Supprimer</button></td>  
+                                            <td className="border px-4 py-2">
+                                                <button
+                                                    onClick={() => this.deleteMessage(contact)} 
+                                                    class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                                                    Supprimer
+                                                </button>
+                                            </td>  
                                             
                                         </tr>
                                     )
