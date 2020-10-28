4eab70a0c463552cc490572483ee31088a7d42e1
jean-claude07 <jeclaude78@gmail.com>
2020-10-28 14:57:25 +0300

    location

--- front/src/Pages/HomePages/Components/Search/Search.js
+++ front/src/Pages/HomePages/Components/Search/Search.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from '../../../../axios';
 import Reservation from '../Reservation/Reservation';
+import { Field } from 'redux-form';
+import moment from 'moment';
+import DatePicker from 'react-datepicker';
+
 
 //import component datepicker et timepicker
 import Datepicker from './Datepicker';
@@ -10,131 +14,48 @@ import './Style.css';
 
 
 export default class Search extends React.Component {
-
-	// constructor() {
-	// 	super();
-	// 	this.state = {
-	// 		lieu_depart: 'Aéroport Rolland-Garros',
-	// 		date_depart: '',
-	// 		heure_depart: '',
-	// 		lieu_retour: 'Aéroport Rolland-Garros',
-	// 		date_retour: '',
-	// 		heure_retour: ''
-	// 	};
-
-	// 	this.handleLieuDepartChange = this.handleLieuDepartChange.bind(this);
-	// 	this.handleDateDepartChange = this.handleDateDepartChange.bind(this);
-	// 	this.handleHeureDepartChange = this.handleHeureDepartChange.bind(this);
-	// 	this.handleLieuRetourChange = this.handleLieuRetourChange.bind(this);
-	// 	this.handleDateRetourChange = this.handleDateRetourChange.bind(this);
-	// 	this.handleHeureRetourChange = this.handleHeureRetourChange.bind(this);
-	// 	this.handleSubmit = this.handleSubmit.bind(this);
-	// }
-	state = {
-		lieu_depart: '',
-	};
-
-
-
-
-	handleLieuDepartChange = event => {
-		this.setState({ lieu_depart: event.target.value });
+	constructor(propos) {
+		super(propos)
 	}
-
-	// handleDateDepartChange(event) {
-	// 	this.setState({
-	// 		date_depart: event.target.value
-	// 	})
-	// }
-
-	// handleHeureDepartChange(event) {
-	// 	this.setState({ Heure_depart: event.target.value });
-	// }
-	// handleLieuRetourChange(event) {
-	// 	this.setState({ lieu_retour: event.target.value });
-	// }
-
-	// handleDateRetourChange(event) {
-	// 	this.setState({
-	// 		date_depart: event.target.value
-	// 	})
-	// }
-	// handleHeureRetourChange(event) {
-	// 	this.setState({ Heure_retour: event.target.value });
-	// }
-
-	handleSubmit = event => {
-		event.preventDefault();
-
-		const reservation = {
-			lieu_depart: this.state.lieu_depart,
-			// date_depart: this.state.date_depart,
-			// heure_depart: this.state.heure_depart,
-			// lieu_retour: this.state.lieu_retour,
-			// date_retour: this.state.date_retour,
-			// heure_retour: this.state.heure_retour,
-		};
-		axios.post('/reservations', { reservation }).then(response => {
-			console.log(response);
-			console.log(response.data);
-		})
-	};
-	// handleSubmit = (values, { resetForm }) => {
-	// 		const { action } = this.props;
-	// 		if (response.status === 201) {
-	// 			resetForm();
-	// 			action.getReservation();
-	// 		} 
-	// 	})
-	// }
-
-	//bouton louer et afficher la page reservation
-	state = {
-		etape: 1
-	}
-
-	changerEtape = (newEtape) => {
-		this.setState({
-			etape: newEtape
-		});
-	}
-
-
-
+	
 	render() {
-		const etape = this.state.etape;
-
+	
+			const { handleSubmit } = this.props;
+	 
+			const renderDatePicker = ({input}) => (
+			  <div>
+					<DatePicker {...input} dateForm="MM/DD/YYYY" selected={input.value ? moment(input.value, 'MM/DD/YYYY') : null} />
+			  </div>
+			);
+	 
 		return (
 			<>
 
 				<div className="container">
 
 					<div className="b-search__main wow zoomInUp" data-wow-delay="0.3s">
+					
 						<h4>Pour louer une voiture à la Réunion, rien de plus simple !<br /> Réservez votre voiture en ligne et pas chère avec GP Location.</h4>
-						<form className="b-search__main-form" onSubmit={this.handleSubmit}>
+						<form className="b-search__main-form">
 							<div className="row">
 								<div className="col-xs-12 col-md-8">
 									<div className="m-firstSelects">
 										<div className="col-xs-5">
 											<p>Lieu de départ</p>
-											<select value={this.state.value} name="select1" onChange={this.handleLieuDepartChange}>
+											<select >
 												<option selected value="0" >Aéroport Roland-Garros</option>
 											</select>
-											{/* <span className="fa fa-caret-down"></span> */}
+											
 
 										</div>
 										<div className="col-xs-3">
 											<p>Date de départ</p>
-											<Datepicker />
-
-											{/* <span className="fa fa-caret-down"></span> */}
-
+											<Field name="date" component={renderDatePicker} />
 										</div>
 										<div className="col-xs-3">
 											<p>heure de départ</p>
-											<Timepicker />
-											{/* <span className="fa fa-caret-down"></span> */}
-
+											<Timepicker name="heure_de_depart" />
+										
 										</div>
 									</div>
 									<div className="m-secondSelects">
@@ -143,23 +64,16 @@ export default class Search extends React.Component {
 											<select name="select1" >
 												<option value="0" >Aéroport Roland-Garros</option>
 											</select>
-											{/* <span className="fa fa-caret-down"></span> */}
-
+										
 										</div>
 										<div className="col-xs-3">
 											<p>date de retour</p>
-											<Datepicker />
-
-
-
-											{/* <span className="fa fa-caret-down"></span> */}
+											<Datepicker name="date_de_retour" />
 
 										</div>
 										<div className="col-xs-3">
 											<p>heure de retour</p>
-											<Timepicker />
-
-											{/* <span className="fa fa-caret-down"></span> */}
+											<Timepicker name="heure_de_retour" />
 
 										</div>
 									</div>
@@ -170,18 +84,19 @@ export default class Search extends React.Component {
 									</div>
 									<div className="b-search__main-form-submit">
 
-										<button onClick={() => this.changerEtape(2)} type="submit" className="btn m-btn">LOUER<span className="fa fa-angle-right"></span></button>
+										<button type="submit" className="btn m-btn">LOUER<span className="fa fa-angle-right"></span></button>
 									</div>
 								</div>
 							</div>
 						</form>
+				
 					</div>
-					{etape === 2 ? (<Reservation />) : null}
+
 				</div>
 
 
 			</>
-		)
+		);
 	}
 }
 
