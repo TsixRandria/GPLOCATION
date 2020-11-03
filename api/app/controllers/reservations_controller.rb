a8f5e9863109fe841bab2ab4c68947fe91c4a0ae
jean-claude07 <jeclaude78@gmail.com>
2020-11-03 07:29:34 +0300

    reservation

--- api/app/controllers/reservations_controller.rb
+++ api/app/controllers/reservations_controller.rb
@@ -1,5 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :update, :destroy]
+  after_action :filtre
 
   # GET /reservations
   def index
@@ -23,6 +24,23 @@ class ReservationsController < ApplicationController
       render json: @reservation.errors, status: :unprocessable_entity
     end
   end
+
+
+  def filtre
+    # return nil if :dateDepart === :dateRetour
+    #   render json: {messsage: "Pour louer aujourd\'hui, merci de contacter directement Cargo Location Perpignan au 04 68 35 86 35."}, status: 202
+
+    # return nil if (:dateDepart <=> :dateRetour) > 90
+    #   render json: {message: "Pour une location plus de 90 jours, veuiller nous contacter via notre formulaire de contact"}, status: 203
+    
+    return false if :dateDepart > :dateRetour
+      render json: {message: "veuiller valider une autre date s'il vous plait"}, status: 430
+    # if :dateDepart === :dateRetour
+    #   render json: {messsage: "Pour louer aujourd\'hui, merci de contacter directement Cargo Location Perpignan au 04 68 35 86 35."}, status: 430
+    # end
+    
+  end
+  
 
   # PATCH/PUT /reservations/1
   def update
