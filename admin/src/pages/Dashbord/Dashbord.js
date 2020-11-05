d6789b10a519fd16c9b73717e2091d82592e9ec5
jean-claude07 <jeclaude78@gmail.com>
2020-11-05 10:01:43 +0300

    button

---
+++ admin/src/pages/Dashbord/Dashbord.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+
+
+export class Dashbord extends Component {
+    render() {
+        return (
+            <div>
+                bonjour 
+            </div>
+        )
+    }
+}
+
+export default Dashbord
+
+
+
+
+
+
+
+  
+
