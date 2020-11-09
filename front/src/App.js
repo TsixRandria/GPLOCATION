7eab9a203d6edeca5ecc3ea2652cdb5a69fe5c90
ericsman <ericlalainar@gmail.com>
2020-11-09 16:18:26 +0300

    profil

--- front/src/App.js
+++ front/src/App.js
@@ -10,6 +10,7 @@ import Aeroport from './Pages/Aeroport/Aeroport.js';
 import Saint from './Pages/Saint/Saint.js';
 import Condition from './Pages/Condition/Condition.js';
 import Reserver from './Pages/HomePages/Components/Reservation/Reserver/Reserver.js';
+import Profil from './Pages/Login/Profil/Profil';
 
 
 import Mentions from './Pages/Mentions/Mentions.js';
@@ -33,6 +34,7 @@ function App() {
         <Route exact path='/Condition' component={Condition}/>
         <Route exact path='/Mentions-legales' component={Mentions}/>
         <Route exact path='/Reserver' component={Reserver}/>
+        <Route exact path='/profil' component={Profil}/>
           <Footer />  
     </BrowserRouter>
   );
