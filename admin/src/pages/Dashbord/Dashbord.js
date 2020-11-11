9c86386d67ebdffef7589df75e63899bd09d804b
ericsman <ericlalainar@gmail.com>
2020-11-11 14:00:04 +0300

    planing

--- admin/src/pages/Dashbord/Dashbord.js
+++ admin/src/pages/Dashbord/Dashbord.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-
+import '/home/eric/Bureau/gplocation/admin/src/pages/Dashbord/Dashdord.css';
 
 
 export class Dashbord extends Component {
     render() {
         return (
             <div>
-                bonjour 
+            bonjour
             </div>
         )
     }
--- admin/src/pages/Planning/Planning.js
+++ admin/src/pages/Planning/Planning.js
@@ -1,23 +1,74 @@
 import React, { Component } from 'react'
-// import BigCalendar from 'react-big-calendar'
-// import moment from 'moment'
+import './Planning.css';
 
-// const localizer = BigCalendar.momentLocalizer(moment) 
 
 export class Planning extends Component {
-  render() {
-    return (
-      <div>
-        {/* <BigCalendar
-          localizer={localizer}
-          events={myEventsList}
-          startAccessor="start"
-          endAccessor="end"
-        /> */}
-        bonjour planing
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="contenu">
+              <div className="right_col" role="main">
+          <div className="">
+            <div className="page-title">
+              <div className="title_left">
+                <h3>Calendar <small>Ajouter des évenement</small></h3>
+              </div>
+
+              <div className="title_right">
+                <div className="col-md-5 col-sm-5   form-group pull-right top_search">
+                  <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Search for..." />
+                    <span className="input-group-btn">
+                      <button className="btn btn-default" type="button">Go!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+{/* 
+            <div className="clearfix"></div> */}
+
+            <div className="row">
+              <div className="col-md-12">
+                <div className="x_panel">
+                  <div className="x_title">
+                    <h2>Calendar Events <small>Sessions</small></h2>
+                    <ul className="nav navbar-right panel_toolbox">
+                      <li><a className="collapse-link"><i className="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li className="dropdown">
+                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i className="fa fa-wrench"></i></a>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a className="dropdown-item" href="#">Settings 1</a>
+                            <a className="dropdown-item" href="#">Settings 2</a>
+                          </div>
+                      </li>
+                      <li><a className="close-link"><i className="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div className="clearfix"></div>
+                  </div>
+                  <div className="x_content">
+
+                    <div id='calendar'></div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+            </div>
+        )
+    }
 }
 
-export default Planning
+export default Planning;
+
+
+
+
+
+
+
+  
+
